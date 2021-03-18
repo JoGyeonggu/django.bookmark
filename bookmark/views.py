b05b7e13a7b398eba0dc7e1234193ddb72f97784
JoGyeonggu <wh941104@gmail.com>
2021-03-18 21:49:57 +0900

    views 'add' Add

--- bookmark/views.py
+++ bookmark/views.py
@@ -8,8 +8,16 @@ from django.shortcuts import render
 # 웹 페이지에 접속한다. -> 페이지를 본다.
 # URL을 입력 -> 웹 서버가 뷰를 찾아서 동작시킨다. -> 응답
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Bookmark
 
 class BookmarkListView(ListView):
     model = Bookmark
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'
 
